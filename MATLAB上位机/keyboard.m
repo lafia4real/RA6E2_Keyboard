ebfae1f6288e2fc5e764b27969283029e45cd6e1
lafia4real <1650374245@qq.com>
2025-12-16 23:42:26 +0800

    绘制了V1.0版本PCB以及增加MATLAB上位机文件

---
+++ MATLAB上位机/keyboard.m
@@ -0,0 +1,87 @@
+function varargout = keyboard(varargin)
+% KEYBOARD MATLAB code for keyboard.fig
+%      KEYBOARD, by itself, creates a new KEYBOARD or raises the existing
+%      singleton*.
+%
+%      H = KEYBOARD returns the handle to a new KEYBOARD or the handle to
+%      the existing singleton*.
+%
+%      KEYBOARD('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in KEYBOARD.M with the given input arguments.
+%
+%      KEYBOARD('Property','Value',...) creates a new KEYBOARD or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before keyboard_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to keyboard_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help keyboard
+
+% Last Modified by GUIDE v2.5 16-Dec-2025 23:09:28
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @keyboard_OpeningFcn, ...
+                   'gui_OutputFcn',  @keyboard_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before keyboard is made visible.
+function keyboard_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to keyboard (see VARARGIN)
+
+% Choose default command line output for keyboard
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes keyboard wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = keyboard_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+set(handles.text_change,'String','按钮被按下了捏');
+
+% --- Executes during object creation, after setting all properties.
+function pushbutton1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
